96fa84fdb961c49d8ec9e247862f711230dfc9a9
Laurent Guillou <laurent29190@hotmail.fr>
2022-02-17 11:05:17 +0100

    plus safe

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
-    require_once 'bdd.php';
-    $bdd = bdd();
+
+require ('vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 echo 'Hello World !' . '<br>';
 
-$waiters = waiters();
+$path = 'mysql:host=' . $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8';
 
-foreach($waiters as $waiter) {
-    echo $waiter['name'] . '<br>';
+$pdo = new PDO (
+            $path,
+            $_ENV['DB_USERNAME'],
+            $_ENV['DB_PASSWORD'],
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+        );
+$sqlQuery = 'SELECT name FROM waiter';
+$execQuery = $pdo->query($sqlQuery);
+
+$waiters = $execQuery->fetchAll();
+
+foreach ($waiters as $waiter) {
+    print $waiter['name'] . '<br>';
 }
